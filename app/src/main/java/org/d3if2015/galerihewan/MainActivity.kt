c17ae5d9c6ce9dc2124642916c36d4790d2f8ef9
PramudiaPutra <pramudiaputra.iv@gmail.com>
2021-03-11 18:39:01 +0700

    Initial commit

---
+++ app/src/main/java/org/d3if2015/galerihewan/MainActivity.kt
@@ -0,0 +1,16 @@
+package org.d3if2015.galerihewan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
