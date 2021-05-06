e9b2f4b8cf994457cac9c64896d1061d84de414e
Pramudia_Putra_Pamungkas <pramudiaputra.iv@gmail.com>
2021-05-06 17:02:43 +0700

    Menambahkan loading indicator

--- app/src/main/java/org/d3if2015/galerihewan/ui/main/MainFragment.kt
+++ app/src/main/java/org/d3if2015/galerihewan/ui/main/MainFragment.kt
@@ -12,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView
 import org.d3if2015.galerihewan.model.Hewan
 import org.d3if2015.galerihewan.R
 import org.d3if2015.galerihewan.databinding.FragmentMainBinding
+import org.d3if2015.galerihewan.network.ApiStatus
 
 class MainFragment : Fragment() {
 
@@ -41,6 +42,25 @@ class MainFragment : Fragment() {
 
         viewModel.getData().observe(viewLifecycleOwner) {
             myAdapter.updateData((it))
+        }
+
+        viewModel.getStatus().observe(viewLifecycleOwner) {
+            updateProgress(it)
+        }
+    }
+
+    private fun updateProgress(status: ApiStatus) {
+        when (status) {
+            ApiStatus.LOADING -> {
+                binding.progressBar.visibility = View.VISIBLE
+            }
+            ApiStatus.SUCCESS -> {
+                binding.progressBar.visibility = View.GONE
+            }
+            ApiStatus.FAILED -> {
+                binding.progressBar.visibility = View.GONE
+                binding.networkError.visibility = View.VISIBLE
+            }
         }
     }
 }
--- app/src/main/java/org/d3if2015/galerihewan/ui/main/MainViewModel.kt
+++ app/src/main/java/org/d3if2015/galerihewan/ui/main/MainViewModel.kt
@@ -6,14 +6,15 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
-import org.d3if2015.galerihewan.R
 import org.d3if2015.galerihewan.model.Hewan
+import org.d3if2015.galerihewan.network.ApiStatus
 import org.d3if2015.galerihewan.network.HewanApi
 import java.lang.Exception
 
 class MainViewModel : ViewModel() {
 
     private val data = MutableLiveData<List<Hewan>>()
+    private val status = MutableLiveData<ApiStatus>()
 
     init {
         retrieveData()
@@ -21,13 +22,18 @@ class MainViewModel : ViewModel() {
 
     private fun retrieveData() {
         viewModelScope.launch {
+            status.value = ApiStatus.LOADING
             try {
                 data.value = HewanApi.service.getHewan()
+                status.value = ApiStatus.SUCCESS
             } catch (e: Exception) {
                 Log.d("MainViewModel", "failure: ${e.message}")
+                status.value = ApiStatus.FAILED
             }
         }
     }
 
     fun getData(): LiveData<List<Hewan>> = data
+
+    fun getStatus(): LiveData<ApiStatus> = status
 }
